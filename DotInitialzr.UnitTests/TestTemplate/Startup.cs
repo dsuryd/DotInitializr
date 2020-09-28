59c7ce65f9e0d3e63125a1b6057281ecaf5be927
Dicky Suryadi <dsuryd@gmail.com>
2020-09-28 16:58:46 -0700

    Add test template.

---
+++ DotInitialzr.UnitTests/TestTemplate/Startup.cs
@@ -0,0 +1,94 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+{{#react}}
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+{{/react}}
+{{#ng}}
+using Microsoft.AspNetCore.SpaServices.AngularCli;
+{{/ng}}
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace {{namespace}}
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            // In production, the Angular files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+{{#react}}	
+                configuration.RootPath = "ClientApp/build";	
+{{/react}}	
+{{#ng}}			
+                configuration.RootPath = "ClientApp/dist";
+{{/ng}}				
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+{{#ng}}					
+            if (!env.IsDevelopment())
+            {
+{{/ng}}					
+                app.UseSpaStaticFiles();
+{{#ng}}									
+            }
+{{/ng}}								
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action=Index}/{id?}");
+            });
+
+            app.UseSpa(spa =>
+            {
+                // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                // see https://go.microsoft.com/fwlink/?linkid=864501
+
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+{{#react}}
+                    spa.UseReactDevelopmentServer(npmScript: "start");
+{{/react}}				
+{{#ng}}				
+                    spa.UseAngularCliServer(npmScript: "start");
+{{/ng}}					
+                }
+            });
+        }
+    }
+}
